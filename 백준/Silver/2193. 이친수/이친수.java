25a2df19e164bcefa7651f76540486876b295d57
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2025-07-27 16:22:12 +0900

    [Silver III] Title: 이친수, Time: 104 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	private static int N;
+	private static long[] dp; // long으로 변경
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		
+		dp = new long[N + 1]; // long 배열로 초기화
+		
+		dp[1] = 1;
+		
+		if (N >= 2) {
+			dp[2] = 1;
+			
+			for (int i = 3; i <= N; i++) {
+			    dp[i] = dp[i - 1] + dp[i - 2];
+			}
+		}
+		
+		System.out.println(dp[N]);
+	}
+}
