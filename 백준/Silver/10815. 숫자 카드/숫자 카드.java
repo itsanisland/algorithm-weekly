154b2f4c9454dfc3f481a07fb52010a621346984
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2025-09-04 19:56:27 +0900

    [Silver V] Title: 숫자 카드, Time: 892 ms, Memory: 157272 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	private static int N, M;
+	private static Map<Integer, Integer> map;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		map = new HashMap<>();
+		
+		for (int i = 0; i < N; i++) {
+			map.putIfAbsent(Integer.parseInt(st.nextToken()), 1);
+		}
+		
+		M = Integer.parseInt(br.readLine());
+		
+		st = new StringTokenizer(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i = 0; i < M; i++) {
+			sb.append(map.getOrDefault(Integer.parseInt(st.nextToken()), 0) + " ");
+		}
+		
+		System.out.println(sb);
+	}
+
+}
