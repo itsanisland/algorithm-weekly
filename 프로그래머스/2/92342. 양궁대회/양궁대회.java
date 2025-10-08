9e2d8413482c7a40b47adb8746a756066977867e
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2025-10-08 17:28:04 +0900

    [level 2] Title: 양궁대회, Time: 0.78 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -1,73 +1,59 @@
 import java.util.*;
 
 class Solution {
-    static int N, maxMinus;
-    static int[] arr;
-    static int[] answer = {-1};
+    int maxDiff = 0;
+    int[] answer;
+    
     public int[] solution(int n, int[] info) {
-        N = n;
-        maxMinus = -1;
-        arr = new int[11];
-        dfs(info, 0, 0);
-
-        return answer;
+        answer = new int[11];
+        dfs(info, new int[11], n, 0);
+        
+        return maxDiff == 0 ? new int[] {-1} : answer;
     }
     
-    //idx는 점수 0~10까지 접근, cnt는 사용한 화살 수
-    private static void dfs(int[] apeach, int idx, int cnt) {
-        if(idx == 11) { //점수 접근을 다 했으면
-            //화살 다 썼는지 확인하고 다 썼으면 점수 계산
-            if(cnt == N) {
-                int apScore = 0, liScore = 0;
-                for(int i = 0; i<11; i++) {
-                    if(apeach[i] == 0 && arr[i] == 0) {
-                        continue;
-                    }
-                    if(apeach[i]>=arr[i]) apScore += 10-i;
-                    else liScore += 10-i;
-                }
-                
-                if(liScore>apScore) {
-                    //라이언이 가장 큰 차이로 이기는 경우
-                    if(liScore-apScore > maxMinus) {
-                        maxMinus = liScore-apScore;
-                        answer = arr.clone();
-                    }
-                    //라이언이 가장 큰 점수 차이로 우승할 수 있는 방법이 여러 가지 일 경우
-                    else if(liScore-apScore == maxMinus) {
-                        for(int i = 10; i>=0; i--) {
-                            if(answer[i]<arr[i]) {
-                                answer = arr.clone();
-                                return;
-                            }
-                            else if(answer[i]>arr[i]) return;
-                        }
-                    }
-                }
-            }
+    private void dfs(int[] aInfo, int[] rInfo, int n, int i) {
+        if (i == 11) {
+            rInfo[10] += n;
+            evaluate(aInfo, rInfo);
+            rInfo[10] -= n;
             return;
         }
-        
-        //둘다 0으로 점수 받기 포기
-        if(apeach[idx] == 0) {
-            dfs(apeach, idx+1, cnt);
+
+        int cnt = aInfo[i] + 1;  // 이기기 위해 필요한 화살 수
+
+        // 이 점수에서 이길 수 있을 때만
+        if (n >= cnt) {
+            rInfo[i] = cnt;
+            dfs(aInfo, rInfo, n - cnt, i + 1);
+            rInfo[i] = 0; // 백트래킹
         }
-        
-        //어피치 이기기
-        if(cnt+1+apeach[idx] <= N) { //현재까지 사용한 화살 수+1에 어피치 화살 수를 더해도 전체 화살 수가 넘지 않으면
-            arr[idx] = apeach[idx]+1;
-            dfs(apeach, idx+1, cnt+1+apeach[idx]);
-            arr[idx] = 0;
+
+        // 이 점수 포기
+        dfs(aInfo, rInfo, n, i + 1);
+    }
+    
+    private void evaluate(int[] aInfo, int[] rInfo) {
+        int rScore = 0, aScore = 0;
+        for (int j = 0; j < 11; j++) {
+            if (rInfo[j] == 0 && aInfo[j] == 0) continue;
+            if (rInfo[j] > aInfo[j]) rScore += 10 - j;
+            else aScore += 10 - j;
         }
-        
-        //어피치한테 지기
-        if(apeach[idx] != 0) {
-            for(int i = 0; i<=apeach[idx]; i++) {
-                arr[idx] = i;
-                dfs(apeach, idx+1, cnt+i);
-                arr[idx] = 0;
-            }
+
+        int diff = rScore - aScore;
+        if (diff <= 0) return;
+
+        if (diff > maxDiff || (diff == maxDiff && isBetter(rInfo, answer))) {
+            maxDiff = diff;
+            answer = rInfo.clone();
         }
-        
+    }
+
+    private boolean isBetter(int[] cur, int[] best) {
+        for (int i = 10; i >= 0; i--) {
+            if (cur[i] > best[i]) return true;
+            else if (cur[i] < best[i]) return false;
+        }
+        return false;
     }
 }
