84b8403d1ff92f850d58b17b38ea8c0db5a562b7
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2025-10-31 19:32:45 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 3.32 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -1,11 +1,13 @@
+import java.util.*;
+import java.util.stream.*;
+
 class Solution {
     public int solution(String myString, String pat) {
-        String str = "";
-        
-        for (char c : myString.toCharArray()) {
-            str += c == 'A' ? 'B' : 'A';
-        }
-        
+        String str = myString.chars()                               // IntStream (각 문자의 유니코드 값)
+                    .mapToObj(c -> (char)(c == 'A' ? 'B' : 'A'))    // char 변환
+                    .map(String::valueOf)                           // String으로 변환
+                    .collect(Collectors.joining());                 // 문자열 합치기
+        System.out.println(str);
         int answer = str.contains(pat) ? 1 : 0;
         return answer;
     }
