3dd7a616cdfbbcf2bf9485680197060e5e18f0ba
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2025-11-16 22:29:55 +0900

    [Bronze IV] Title: 세수정렬, Time: 152 ms, Memory: 16348 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[3];
+        
+        arr[0] = Integer.parseInt(st.nextToken());
+        arr[1] = Integer.parseInt(st.nextToken());
+        arr[2] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arr);
+
+        System.out.println(arr[0] + " " + arr[1] + " " + arr[2]);
+    }
+}
