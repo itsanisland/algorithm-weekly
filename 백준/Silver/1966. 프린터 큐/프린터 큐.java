3a3bc249f59b276fa9603316f95f7a5624fc5e31
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2025-11-19 00:19:47 +0900

    [Silver III] Title: 프린터 큐, Time: 124 ms, Memory: 14616 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.java
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -1,58 +1,45 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Collections;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Main {
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+   
+        int t = Integer.parseInt(br.readLine());
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+        while (t-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int T = Integer.parseInt(st.nextToken());
-		
-		for (int tc = 0; tc < T; tc++) {
-			st = new StringTokenizer(br.readLine());
-			
-			int N = Integer.parseInt(st.nextToken());
-			int M = Integer.parseInt(st.nextToken());
-			
-			st = new StringTokenizer(br.readLine());
-			
-			Queue<int[]> queue = new ArrayDeque<>();
-			PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
 
-			
-			for (int i = 0; i < N; i++) {
-				int priority = Integer.parseInt(st.nextToken());
-			    queue.offer(new int[] {i, priority});
-			    pq.offer(priority);
-			}
+            st = new StringTokenizer(br.readLine());
 
-			int result = 0;
-			
-			while (!queue.isEmpty()) {
-				int[] current = queue.poll();
-				int idx = current[0];
-				int priority = current[1];
-				
-				if (priority == pq.peek()) {
-					result++;
-					pq.poll();
-					if (idx == M) {
-						break;
-					}
-				} else {
-					queue.offer(current);
-				}
-			}
-			
-			System.out.println(result);
-		}
-	}
+            PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+            Queue<int[]> q = new ArrayDeque<>();
 
+            for (int i = 0; i < n; i++) {
+                int data = Integer.parseInt(st.nextToken());
+                pq.offer(data);
+                q.offer(new int[] { i, data });
+            }
+
+            int cnt = 1;
+            while (!q.isEmpty()) {
+                int[] qPeek = q.peek();
+                if (pq.peek() == qPeek[1]) {
+                    if (qPeek[0] == m) {
+                        System.out.println(cnt);
+                        break;
+                    }
+
+                    cnt++;
+                    pq.poll();
+                    q.poll();
+                } else {
+                    q.offer(q.poll());
+                }
+            }
+        }
+    }
 }
