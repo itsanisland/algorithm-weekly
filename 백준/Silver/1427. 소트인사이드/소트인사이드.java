b69f9033313056041d02ea9c602eb66cd5d29474
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2025-11-20 22:42:57 +0900

    [Silver V] Title: 소트인사이드, Time: 132 ms, Memory: 16272 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		char[] nums = br.readLine().toCharArray();
+		
+		Arrays.sort(nums);
+		
+		String ans = "";
+		for (int i = nums.length - 1; i >= 0; i--) {
+			ans += nums[i];
+		}
+
+		System.out.println(ans);
+	}
+
+}
