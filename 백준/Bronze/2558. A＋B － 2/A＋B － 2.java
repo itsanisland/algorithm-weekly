8cb05eacddb0014c9853b1b59d525587d6d1b813
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2025-11-21 19:07:30 +0900

    [Bronze V] Title: A+B - 2, Time: 104 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -3,8 +3,6 @@ import java.io.*;
 
 class Main {
 
-    private static int N;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
