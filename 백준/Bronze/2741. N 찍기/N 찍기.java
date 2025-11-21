a3f4d95ac66d02777a93e2b1f97fab0447828a35
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2025-11-21 19:06:07 +0900

    [Bronze V] Title: N 찍기, Time: 808 ms, Memory: 31716 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+    private static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+    }
+}
