f68da7f7bd1d3f9837efc545f1bc9998aa345305
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2025-11-21 19:06:59 +0900

    [Bronze V] Title: 기찍 N, Time: 760 ms, Memory: 30684 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = n; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
