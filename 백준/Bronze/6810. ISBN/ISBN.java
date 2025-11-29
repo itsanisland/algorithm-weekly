8e0a167087b435fd2fcc933d8678325ac66ca6b3
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2025-11-29 22:57:21 +0900

    [Bronze IV] Title: ISBN, Time: 112 ms, Memory: 14584 KB -BaekjoonHub

---
+++ 백준/Bronze/6810. ISBN/ISBN.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+
+        System.out.println("The 1-3-sum is " + (91 + a + b * 3 + c));
+    }
+}
