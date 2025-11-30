e40281b14a2f2751f739546fd7e83c03aad3f636
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2025-11-30 22:36:59 +0900

    [Bronze V] Title: 등록, Time: 92 ms, Memory: 14056 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("397");
+        System.out.println("itsanisland");
+    }
+}
