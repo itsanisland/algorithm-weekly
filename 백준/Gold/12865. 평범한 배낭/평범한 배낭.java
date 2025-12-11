c9de2a41f926ee54180e79a40bfb16f8a4b2a948
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2025-12-11 16:41:34 +0900

    [Gold V] Title: 평범한 배낭, Time: 180 ms, Memory: 53832 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+    public static List<Integer>[] graph;
+    public static int[] indegrees;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] w = new int[n + 1];
+        int[] v = new int[n + 1];
+        
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            w[i] = Integer.parseInt(st.nextToken());
+            v[i] = Integer.parseInt(st.nextToken());
+            
+        }
+
+        int[][] dp = new int[n + 1][k + 1];
+
+        for (int i = 1; i <= n; i++) { // 각 물건에 대해
+            for (int j = 1; j <= k; j++) { // dp 배열 한 행씩 업데이트
+                if (j < w[i]) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]);
+                }
+            }
+        }
+        
+        System.out.print(dp[n][k]);
+    }
+}
