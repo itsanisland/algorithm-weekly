355c54fc09dc87dd9ba4baf466a681117ac5855b
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2025-12-14 23:07:05 +0900

    [Bronze V] Title: 연세대학교, Time: 108 ms, Memory: 14100 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());
+
+        System.out.println(n == 0 ? "YONSEI" : "Leading the Way to the Future");
+    }
+}
