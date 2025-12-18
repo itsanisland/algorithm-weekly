5114d69e41cc0b845e8bb0b4d64f81070b41eb38
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2025-12-18 22:05:59 +0900

    [Bronze V] Title: 홍익대학교, Time: 104 ms, Memory: 14076 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(n - 1946);
+    }
+}
