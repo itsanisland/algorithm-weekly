94b404fd1e34dbca4107a08df2751d74ecda6c61
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2025-12-20 22:40:53 +0900

    [Bronze V] Title: 역사를 걸으면 동국이 보이고, Time: 108 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/34721. 역사를 걸으면 동국이 보이고/역사를 걸으면 동국이 보이고.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("I love DGU");
+        }     
+    }
+}
