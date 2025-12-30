f52a08f353b6e24f4fa0ef0c903ddde25f205822
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2025-12-30 17:05:24 +0900

    [Silver II] Title: 유기농 배추, Time: 144 ms, Memory: 15912 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -6,70 +6,69 @@ class Main {
     public final static int[] DY = { -1, 1, 0, 0 };
     public final static int[] DX = { 0, 0, -1, 1 };
 
-    public static int M, N;
-    public static int[][] map;
+    public static int n, m;
+    public static boolean[][] map;
     public static boolean[][] visited;
-    
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int t = Integer.parseInt(br.readLine());
 
-        while (t-- > 0) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            M = Integer.parseInt(st.nextToken());
-            N = Integer.parseInt(st.nextToken());
-            int k = Integer.parseInt(st.nextToken());
-
-            map = new int[N][M];
-            visited = new boolean[N][M];
-
-            for (int i = 0; i < k; i++) {
-                st = new StringTokenizer(br.readLine());
-                int x = Integer.parseInt(st.nextToken());
-                int y = Integer.parseInt(st.nextToken());
-                map[y][x] = 1;
-            }
-
-            int cnt = 0;
-            
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
-                    if (map[i][j] == 1 && !visited[i][j]) {
-                        cnt++;
-                        bfs(i, j);
-                    }
-                }
-            }
-
-            System.out.println(cnt);
-        }
+    public static boolean isValid(int y, int x) {
+        return 0 <= y && y < n && 0 <= x && x < m;
     }
 
     public static void bfs(int y, int x) {
         Queue<int[]> q = new ArrayDeque<>();
         q.offer(new int[] { y, x });
-
         visited[y][x] = true;
 
         while (!q.isEmpty()) {
             int[] cur = q.poll();
             int cy = cur[0];
             int cx = cur[1];
-            
+
             for (int i = 0; i < 4; i++) {
                 int ny = cy + DY[i];
                 int nx = cx + DX[i];
 
-                if (isValid(ny, nx) && map[ny][nx] == 1 && !visited[ny][nx]) {
+                if (isValid(ny, nx) && map[ny][nx] && !visited[ny][nx]) {
                     visited[ny][nx] = true;
                     q.offer(new int[] { ny, nx });
                 }
             }
         }
     }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static boolean isValid(int y, int x) {
-        return 0 <= y && y < N && 0 <= x && x < M;
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            m = Integer.parseInt(st.nextToken());
+            n = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+            
+            map = new boolean[n][m];
+            visited = new boolean[n][m];
+    
+            for (int i = 0; i < k; i++) {
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                map[y][x] = true;
+            }
+
+            int ans = 0;
+            
+            for (int y = 0; y < n; y++) {
+                for (int x = 0; x < m; x++) {
+                    if (map[y][x] && !visited[y][x]) {
+                        ans++;
+                        bfs(y, x);
+                    }
+                }
+            }
+    
+            System.out.println(ans);
+        }
     }
 }
