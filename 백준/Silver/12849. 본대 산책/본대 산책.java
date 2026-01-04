28400746ff47f25ffbaa3e23c4214df6c4b3948b
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-01-04 17:14:31 +0900

    [Silver I] Title: 본대 산책, Time: 120 ms, Memory: 21852 KB -BaekjoonHub

--- 백준/Silver/12849. 본대 산책/본대 산책.java
+++ 백준/Silver/12849. 본대 산책/본대 산책.java
@@ -3,38 +3,32 @@ import java.io.*;
 
 class Main {
 
-    public final static int MOD = 1_000_000_007;
+    public static final long MOD = 1_000_000_007L;
 
-    public static void main(String[] args) throws IOException {
+    public static long[] next(long[] state) {
+        long[] tmp = new long[8];
+        tmp[0] = (state[1] + state[2]) % MOD;
+        tmp[1] = (state[0] + state[2] + state[3]) % MOD;
+        tmp[2] = (state[0] + state[1] + state[3] + state[4]) % MOD;
+        tmp[3] = (state[1] + state[2] + state[4] + state[5]) % MOD;
+        tmp[4] = (state[2] + state[3] + state[5] + state[6]) % MOD;
+        tmp[5] = (state[3] + state[4] + state[7]) % MOD;
+        tmp[6] = (state[4] + state[7]) % MOD;
+        tmp[7] = (state[5] + state[6]) % MOD;
+        return tmp;
+    }
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
         int d = Integer.parseInt(br.readLine());
-        int[][] dp = new int[d + 1][8];
-        List<Integer>[] adj = new ArrayList[8];
-        for (int i = 0; i < 8; i++) {
-            adj[i] = new ArrayList<>();
+
+        long[] dp = new long[8];
+        dp[0] = 1;
+
+        while (d-- > 0) {
+            dp = next(dp);
         }
 
-        adj[0].add(1); adj[0].add(2);
-        adj[1].add(0); adj[1].add(2); adj[1].add(3);
-        adj[2].add(0); adj[2].add(1); adj[2].add(3); adj[2].add(4);
-        adj[3].add(1); adj[3].add(2); adj[3].add(4); adj[3].add(5);
-        adj[4].add(2); adj[4].add(3); adj[4].add(5); adj[4].add(6);
-        adj[5].add(3); adj[5].add(4); adj[5].add(7);
-        adj[6].add(4); adj[6].add(7);
-        adj[7].add(5); adj[7].add(6);
-
-        dp[0][0] = 1;
-
-        for (int i = 1; i <= d; i++) {
-            for (int cur = 0; cur < 8; cur++) {
-                for (int nxt : adj[cur]) {
-                    dp[i][nxt] += dp[i - 1][cur];
-                    dp[i][nxt] %= MOD;
-                }
-            }
-        } 
-
-        System.out.println(dp[d][0]);
+        System.out.println(dp[0] % MOD);
     }
 }
