da893f1c0c52b7482701f1634bc3518704911869
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-01-06 23:00:55 +0900

    [Bronze III] Title: 문자열을 만들어요, Time: 100 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/32297. 문자열을 만들어요/문자열을 만들어요.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+        
+        System.out.println(s.contains("gori") ? "YES" : "NO");
+    }
+}
