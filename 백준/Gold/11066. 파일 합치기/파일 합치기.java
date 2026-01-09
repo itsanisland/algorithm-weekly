b52a3bf0751fbda6fb51950c68a26541cf8d6555
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-01-09 14:51:42 +0900

    [Gold III] Title: 파일 합치기, Time: 480 ms, Memory: 30176 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.java
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.java
@@ -12,34 +12,28 @@ class Main {
             int K = Integer.parseInt(br.readLine());
             StringTokenizer st = new StringTokenizer(br.readLine());
 
-            int[] sum = new int[K + 1];
+            int[] psum = new int[K + 1];
             int[][] dp = new int[K + 1][K + 1];
 
             // prefix sum
             for (int i = 1; i <= K; i++) {
-                sum[i] = sum[i - 1] + Integer.parseInt(st.nextToken());
-            }
-
-            // dp 초기화
-            for (int i = 1; i <= K; i++) {
-                Arrays.fill(dp[i], Integer.MAX_VALUE);
-                dp[i][i] = 0;
+                psum[i] = psum[i - 1] + Integer.parseInt(st.nextToken());
             }
 
             // 구간 길이
-            for (int len = 2; len <= K; len++) {
-                for (int i = 1; i + len - 1 <= K; i++) {
-                    int j = i + len - 1;
-
-                    for (int k = i; k < j; k++) {
-                        dp[i][j] = Math.min(
-                            dp[i][j],
-                            dp[i][k] + dp[k + 1][j]
+            for (int len = 1; len < K; len++) {
+                for (int start = 1; start + len <= K; start++) {
+                    int end = start + len;
+            
+                    dp[start][end] = Integer.MAX_VALUE;
+            
+                    for (int mid = start; mid < end; mid++) {
+                        dp[start][end] = Math.min(
+                            dp[start][end],
+                            dp[start][mid] + dp[mid + 1][end]
+                            + psum[end] - psum[start - 1]
                         );
                     }
-
-                    // 마지막에 구간 합을 한 번만 더함
-                    dp[i][j] += sum[j] - sum[i - 1];
                 }
             }
 
