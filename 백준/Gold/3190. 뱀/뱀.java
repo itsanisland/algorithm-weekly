6454335bc5e2304c4c1e7c80a42df3265acefbea
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-01-13 16:53:40 +0900

    [Gold IV] Title: 뱀, Time: 104 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.java
+++ 백준/Gold/3190. 뱀/뱀.java
@@ -3,78 +3,68 @@ import java.io.*;
 
 class Main {
 
-    public final static int[] DY = { 0, 1, 0, -1 }; // 시계 방향 기준
-    public final static int[] DX = { 1, 0, -1, 0 };
-    
-    public static void main(String[] args) throws IOException {
+    static final int[] DY = {0, 1, 0, -1};
+    static final int[] DX = {1, 0, -1, 0};
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        
+
         int N = Integer.parseInt(br.readLine());
         int K = Integer.parseInt(br.readLine());
-        char[][] B = new char[N + 2][N + 2];
-        int[][] D = new int[N + 2][N + 2]; // 방향
-        
-        Arrays.fill(B[0], '#');
-        Arrays.fill(B[N + 1], '#');
-        for (int i = 1; i < N + 1; i++) {
-            B[i][0] = '#';
-            for (int j = 1; j < N + 1; j++) {
-                B[i][j] = '.';
-            }
-            B[i][N + 1] = '#';
-        }
-        
+
+        int[][] board = new int[N + 1][N + 1]; // 0: 빈칸, 1: 사과
         for (int i = 0; i < K; i++) {
             st = new StringTokenizer(br.readLine());
-            int r = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            B[r][c] = 'A';
+            board[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())] = 1;
         }
 
         int L = Integer.parseInt(br.readLine());
-        char[] C = new char[10_001];
-        
+        char[] turn = new char[10001];
         for (int i = 0; i < L; i++) {
             st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            char c = st.nextToken().charAt(0);
-            C[x] = c;
+            turn[Integer.parseInt(st.nextToken())] = st.nextToken().charAt(0);
         }
 
-        int sec = 0;
-        int hy = 1, hx = 1; // 머리
-        B[hy][hx] = 'S';
-        int dir = 0; // 현재 머리 방향(오른쪽)
-        int ty = 1, tx = 1; // 꼬리
-        
+        Deque<int[]> snake = new ArrayDeque<>();
+        boolean[][] body = new boolean[N + 1][N + 1];
+
+        snake.offerFirst(new int[]{1, 1});
+        body[1][1] = true;
+
+        int y = 1, x = 1, dir = 0, time = 0;
+
         while (true) {
-            if (C[sec] == 'D') {
-                dir++;
-                dir %= 4;
-            } else if (C[sec] == 'L') {
-                dir += 3;
-                dir %= 4;
-            }
-            D[hy][hx] = dir;
+            time++;
 
-            hy += DY[dir]; hx += DX[dir];
+            int ny = y + DY[dir];
+            int nx = x + DX[dir];
 
-            sec++;
-
-            if (B[hy][hx] == '#' || B[hy][hx] == 'S') {
+            // 충돌
+            if (ny < 1 || ny > N || nx < 1 || nx > N || body[ny][nx]) {
                 break;
             }
 
-            if (B[hy][hx] == '.') {
-                B[ty][tx] = '.';
-                int dt = D[ty][tx];
-                ty += DY[dt]; tx += DX[dt];
+            // 머리 이동
+            snake.offerFirst(new int[]{ny, nx});
+            body[ny][nx] = true;
+
+            // 사과가 없으면 꼬리 제거
+            if (board[ny][nx] == 0) {
+                int[] tail = snake.pollLast();
+                body[tail[0]][tail[1]] = false;
+            } else {
+                board[ny][nx] = 0;
             }
-            
-            B[hy][hx] = 'S';
+
+            // 방향 전환
+            if (turn[time] == 'D') dir = (dir + 1) % 4;
+            else if (turn[time] == 'L') dir = (dir + 3) % 4;
+
+            y = ny;
+            x = nx;
         }
 
-        System.out.println(sec);
-    }  
+        System.out.println(time);
+    }
 }
