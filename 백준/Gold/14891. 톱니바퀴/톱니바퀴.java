fec226faf02c30499a2ca710d4a8ea93187aee2e
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-01-18 17:02:26 +0900

    [Gold V] Title: 톱니바퀴, Time: 108 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
@@ -1,73 +1,71 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Main {
 
-    public static char[][] T;
+    static int[] gears = new int[4];
 
-    public static void turn(int num, int dir) {
-        char tmp;
-        if (dir == 1) { // 시계 방향 ->
-            tmp = T[num][7];
-            for (int i = 7; i > 0; i--) {
-                T[num][i] = T[num][i - 1];
-            }
-            T[num][0] = tmp;
-        } else { // 반시계 방향 <-
-            tmp = T[num][0];
-            for (int i = 1; i < 8; i++) {
-                T[num][i - 1] = T[num][i];
-            }
-            T[num][7] = tmp;
+    static void rotate(int idx, int dir) {
+        if (dir == 1) { // 시계
+            int last = gears[idx] & 1;
+            gears[idx] = (gears[idx] >> 1) | (last << 7);
+        } else {        // 반시계
+            int first = (gears[idx] >> 7) & 1;
+            gears[idx] = ((gears[idx] << 1) & 0xFF) | first;
         }
     }
 
-    public static void turnAll(int num, int dir) {
-        // 회전 방향 배열
-        int[] turnDir = new int[4];
-        turnDir[num] = dir;
-
-        // <- num: 왼쪽 전파
-        for (int i = num; i > 0; i--) {
-            // T[i - 1], T[i]
-            if (T[i - 1][2] == T[i][6]) break;
-            turnDir[i - 1] = -turnDir[i];
-        }
-
-        // num ->: 오른쪽 전파
-        for (int i = num; i < 3; i++) {
-            // T[i], T[i + 1]
-            if (T[i][2] == T[i + 1][6]) break;
-            turnDir[i + 1] = -turnDir[i];
-        }
-
-        // 실제 회전
-        for (int i = 0; i < 4; i++) {
-            if (turnDir[i] != 0) turn(i, turnDir[i]);
-        }
+    static int getBit(int gear, int idx) {
+        return (gear >> idx) & 1;
     }
-    
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        T = new char[4][8];
         for (int i = 0; i < 4; i++) {
-            T[i] = br.readLine().toCharArray();
+            String s = br.readLine();
+            int val = 0;
+            for (int j = 0; j < 8; j++) {
+                val = (val << 1) | (s.charAt(j) - '0');
+            }
+            gears[i] = val;
         }
 
         int K = Integer.parseInt(br.readLine());
-        for (int i = 0; i < K; i++) {
+
+        while (K-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int num = Integer.parseInt(st.nextToken()) - 1;
             int dir = Integer.parseInt(st.nextToken());
-            turnAll(num, dir);
+
+            int[] turnDir = new int[4];
+            turnDir[num] = dir;
+
+            // 왼쪽 전파
+            for (int i = num; i > 0; i--) {
+                if (getBit(gears[i], 1) == getBit(gears[i - 1], 5)) break;
+                turnDir[i - 1] = -turnDir[i];
+            }
+
+            // 오른쪽 전파
+            for (int i = num; i < 3; i++) {
+                if (getBit(gears[i], 5) == getBit(gears[i + 1], 1)) break;
+                turnDir[i + 1] = -turnDir[i];
+            }
+
+            // 실제 회전
+            for (int i = 0; i < 4; i++) {
+                if (turnDir[i] != 0) rotate(i, turnDir[i]);
+            }
         }
 
         int score = 0;
         for (int i = 0; i < 4; i++) {
-            score += T[i][0] == '0' ? 0 : 1 << i;
+            if (((gears[i] >> 7) & 1) == 1) {
+                score += (1 << i);
+            }
         }
-        
+
         System.out.println(score);
     }
 }
