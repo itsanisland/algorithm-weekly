af58fdc350d2ca7bf9b0158af34b54883d29a4ca
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-02-03 22:47:23 +0900

    [Gold I] Title: 어른 상어, Time: 140 ms, Memory: 20516 KB -BaekjoonHub

--- 백준/Gold/19237. 어른 상어/어른 상어.java
+++ 백준/Gold/19237. 어른 상어/어른 상어.java
@@ -1,158 +1,140 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
+class Main {
 
     static class Smell {
-        int shark, time;
-        Smell(int shark, int time) {
-            this.shark = shark;
-            this.time = time;
-        }
+        int shark, t = 0;
     }
 
-    static int N, M, K;
-    static int[][] board;
-    static Smell[][] smell;
-    static int[] dir;
-    static int[][][] priority;
-
-    static final int[] dy = {0, -1, 1, 0, 0};
-    static final int[] dx = {0, 0, 0, -1, 1};
-
-    public static void main(String[] args) throws Exception {
+    static final int[] DY = {0, -1, 1, 0, 0};
+    static final int[] DX = {0, 0, 0, -1, 1};
+    
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-        board = new int[N][N];
-        smell = new Smell[N][N];
-        dir = new int[M + 1];
-        priority = new int[M + 1][5][4];
+        int[][] board = new int[N][N];
+        Smell[][] smells = new Smell[N][N];
+        int[] directions = new int[M + 1];
+        int[][][] priorities= new int[M + 1][5][4];
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
                 board[i][j] = Integer.parseInt(st.nextToken());
+                smells[i][j] = new Smell();
                 if (board[i][j] > 0) {
-                    smell[i][j] = new Smell(board[i][j], K);
+                    smells[i][j].shark = board[i][j];
+                    smells[i][j].t = K;
                 }
             }
         }
-
+  
         st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= M; i++) dir[i] = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= M; i++) {
+            directions[i] = Integer.parseInt(st.nextToken());
+        }
 
         for (int i = 1; i <= M; i++) {
-            for (int d = 1; d <= 4; d++) {
+            for (int j = 1; j <= 4; j++) {
                 st = new StringTokenizer(br.readLine());
                 for (int k = 0; k < 4; k++) {
-                    priority[i][d][k] = Integer.parseInt(st.nextToken());
+                    priorities[i][j][k] = Integer.parseInt(st.nextToken());
                 }
             }
         }
 
-        int time = 1;
+        int t = 0;
+        
+        while (++t <= 1000) {
+            int[][] moved = new int[N][N];
 
-        while (time <= 1000) {
-            int[][] next = new int[N][N];
-
-            // 1) 이동
             for (int y = 0; y < N; y++) {
                 for (int x = 0; x < N; x++) {
                     if (board[y][x] == 0) continue;
 
                     int shark = board[y][x];
-                    int d = dir[shark];
-                    boolean moved = false;
+                    int d = directions[shark];
 
-                    // 냄새 없는 칸
-                    for (int k = 0; k < 4; k++) {
-                        int nd = priority[shark][d][k];
-                        int ny = y + dy[nd];
-                        int nx = x + dx[nd];
+                    boolean isMoved = false;
 
-                        if (ny < 0 || nx < 0 || ny >= N || nx >= N) continue;
-                        if (smell[ny][nx] != null) continue;
+                    // 냄새 없는 칸 탐색
+                    for (int i = 0; i < 4; i++) {
+                        int nd = priorities[shark][d][i];
+                        int ny = y + DY[nd];
+                        int nx = x + DX[nd];
+                        
+                        if (ny < 0 || ny >= N || nx < 0 || nx >= N || smells[ny][nx].shark > 0) continue;
+                        
+                        if (moved[ny][nx] == 0 || moved[ny][nx] > shark) {
+                            moved[ny][nx] = shark;
+                        }
 
-                        move(next, shark, ny, nx);
-                        dir[shark] = nd;
-                        moved = true;
+                        directions[shark] = nd;
+                        isMoved = true;
                         break;
                     }
+                         
+                    // 없으면 자기 냄새 탐색
+                    if (!isMoved) {
+                        for (int i = 0; i < 4; i++) {
+                            int nd = priorities[shark][d][i];
+                            int ny = y + DY[nd];
+                            int nx = x + DX[nd];
+                            
+                            if (ny < 0 || ny >= N || nx < 0 || nx >= N || smells[ny][nx].shark != shark) continue;
+                            
+                            if (moved[ny][nx] == 0 || moved[ny][nx] > shark) {
+                                moved[ny][nx] = shark;
+                            }
 
-                    // 자기 냄새
-                    if (!moved) {
-                        for (int k = 0; k < 4; k++) {
-                            int nd = priority[shark][d][k];
-                            int ny = y + dy[nd];
-                            int nx = x + dx[nd];
-
-                            if (ny < 0 || nx < 0 || ny >= N || nx >= N) continue;
-                            if (smell[ny][nx].shark != shark) continue;
-
-                            move(next, shark, ny, nx);
-                            dir[shark] = nd;
+                            directions[shark] = nd;
                             break;
                         }
                     }
                 }
             }
 
-            board = next;
-
-            // 2) 냄새 감소
+            board = moved;
+           
+            // 모든 냄새 감소
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
-                    if (smell[i][j] != null) {
-                        smell[i][j].time--;
-                        if (smell[i][j].time == 0) smell[i][j] = null;
+                    if (smells[i][j].t > 0) {
+                        smells[i][j].t--;
+                        if (smells[i][j].t == 0) {
+                            smells[i][j].shark = 0;
+                        }
                     }
                 }
             }
 
-            // 3) 새 냄새 생성
+            // 이동 완료 후 냄새 남기기
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
                     if (board[i][j] > 0) {
-                        smell[i][j] = new Smell(board[i][j], K);
+                        smells[i][j].shark = board[i][j];
+                        smells[i][j].t = K;
                     }
                 }
             }
 
-            // 종료
             boolean onlyOne = true;
-            for (int i = 2; i <= M; i++) {
-                if (exists(i)) {
-                    onlyOne = false;
-                    break;
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (board[i][j] > 1) {
+                        onlyOne = false;
+                    }
                 }
             }
-
-            if (onlyOne) {
-                System.out.println(time);
-                return;
-            }
-
-            time++;
+            if (onlyOne) break;
         }
-
-        System.out.println(-1);
-    }
-
-    static void move(int[][] next, int shark, int y, int x) {
-        if (next[y][x] == 0 || next[y][x] > shark) {
-            next[y][x] = shark;
-        }
-    }
-
-    static boolean exists(int s) {
-        for (int i = 0; i < N; i++)
-            for (int j = 0; j < N; j++)
-                if (board[i][j] == s) return true;
-        return false;
+        
+        System.out.println(t > 1000 ? -1 : t);
     }
 }
