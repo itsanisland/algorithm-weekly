63c8c3b6b3c7d9e8102d55ff11842035f2099d5b
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-02-18 16:35:16 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -0,0 +1,20 @@
+SELECT EMP_NO, EMP_NAME, GRADE, 
+(CASE
+    WHEN GRADE = 'S' THEN SAL * 0.2
+    WHEN GRADE = 'A' THEN SAL * 0.15
+    WHEN GRADE = 'B' THEN SAL * 0.1
+    ELSE 0
+END) AS BONUS
+FROM (
+    SELECT EMP_NO, EMP_NAME, SAL,
+        (CASE
+            WHEN AVG(SCORE) >= 96 THEN 'S'
+            WHEN AVG(SCORE) >= 90 THEN 'A'
+            WHEN AVG(SCORE) >= 80 THEN 'B'
+            ELSE 'C'
+        END) AS GRADE
+    FROM HR_EMPLOYEES
+    JOIN HR_GRADE USING (EMP_NO)
+    GROUP BY EMP_NO
+) t 
+ORDER BY 1
