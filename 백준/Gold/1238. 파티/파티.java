439541eb6cbfaa471ce4bc803b8472f69a6d10df
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-02-18 09:32:27 +0900

    [Gold III] Title: 파티, Time: 172 ms, Memory: 19468 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -17,13 +17,15 @@ class Main {
     }
 
     static final int INF = Integer.MAX_VALUE;
-    static List<Road>[] roads;
+    static int N, X;
 
-    static void dijkstra(int start, int[] dist) {
+    static int[] dijkstra(List<Road>[] roads) {
+        int[] dist = new int[N + 1];
+        Arrays.fill(dist, INF);
+        dist[X] = 0;
+        
         PriorityQueue<Road> pq = new PriorityQueue<>();
-        pq.offer(new Road(start, 0));
-
-        dist[start] = 0;
+        pq.offer(new Road(X, 0));
 
         while (!pq.isEmpty()) {
             Road cur = pq.poll();
@@ -35,21 +37,22 @@ class Main {
                 }
             }
         }
+
+        return dist;
     }
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int X = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken());
 
-        roads = new ArrayList[N + 1];
+        List<Road>[] roads = new ArrayList[N + 1];
+        List<Road>[] reverseRoads = new ArrayList[N + 1];
         for (int i = 1; i <= N; i++) roads[i] = new ArrayList<>();
-
-        int[] dist = new int[N + 1];
-        Arrays.fill(dist, INF);
+        for (int i = 1; i <= N; i++) reverseRoads[i] = new ArrayList<>();
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
@@ -57,17 +60,16 @@ class Main {
             int e = Integer.parseInt(st.nextToken());
             int t = Integer.parseInt(st.nextToken());
             roads[s].add(new Road(e, t));
+            reverseRoads[e].add(new Road(s, t));
         }
 
-        dijkstra(X, dist);
+        int[] dist1 = dijkstra(roads);
+        int[] dist2 = dijkstra(reverseRoads);
 
-        int[] dist2 = new int[N + 1];
         int max = 0;
 
         for (int i = 1; i <= N; i++) {
-            Arrays.fill(dist2, INF);
-            dijkstra(i, dist2);
-            max = Math.max(max, dist[i] + dist2[X]);
+            max = Math.max(max, dist1[i] + dist2[i]);
         }
 
         System.out.println(max);
