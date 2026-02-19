0d6fce62f5160ebdca939c578ebc27721411046a
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-02-19 22:02:09 +0900

    [level 2] Title: 완전범죄, Time: 0.33 ms, Memory: 91 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.java
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.java
@@ -2,40 +2,43 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] info, int n, int m) {
-        boolean[][] dp = new boolean[n][m];
-        dp[0][0] = true;
+        final int INF = Integer.MAX_VALUE;
+        
+        int[] dp = new int[m];
+        Arrays.fill(dp, INF);
+        dp[0] = 0;
 
         for (int[] cur : info) {
             int aInc = cur[0];
             int bInc = cur[1];
 
-            boolean[][] next = new boolean[n][m];
+            int[] next = new int[m];
+            Arrays.fill(next, INF);
 
-            for (int a = 0; a < n; a++) {
-                for (int b = 0; b < m; b++) {
-                    if (!dp[a][b]) continue;
+            for (int b = 0; b < m; b++) {
+                if (dp[b] == INF) continue;
 
-                    // A팀이 맡음
-                    if (a + aInc < n) {
-                        next[a + aInc][b] = true;
-                    }
+                // A가 맡음
+                int newA = dp[b] + aInc;
+                if (newA < n) {
+                    next[b] = Math.min(next[b], newA);
+                }
 
-                    // B팀이 맡음
-                    if (b + bInc < m) {
-                        next[a][b + bInc] = true;
-                    }
+                // B가 맡음
+                int newB = b + bInc;
+                if (newB < m) {
+                    next[newB] = Math.min(next[newB], dp[b]);
                 }
             }
 
             dp = next;
         }
 
-        for (int a = 0; a < n; a++) {
-            for (int b = 0; b < m; b++) {
-                if (dp[a][b]) return a;
-            }
+        int ans = INF;
+        for (int b = 0; b < m; b++) {
+            ans = Math.min(ans, dp[b]);
         }
-        
-        return -1;
+
+        return ans == INF ? -1 : ans;
     }
 }
