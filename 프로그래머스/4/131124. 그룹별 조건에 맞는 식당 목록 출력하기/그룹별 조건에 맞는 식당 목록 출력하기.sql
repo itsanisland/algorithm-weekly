4492e1f553bc930376a23c95af0a3581f5d181de
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-02-20 15:11:34 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,11 +1,29 @@
-SELECT MEMBER_NAME, REVIEW_TEXT, DATE_FORMAT(REVIEW_DATE, '%Y-%m-%d') AS REVIEW_DATE
-FROM (
-        SELECT MEMBER_ID, MEMBER_NAME, COUNT(1) AS CNT
-        FROM MEMBER_PROFILE
-        JOIN REST_REVIEW USING (MEMBER_ID)
-        GROUP BY MEMBER_ID
-        ORDER BY CNT DESC
-        LIMIT 1
-    ) T
-JOIN REST_REVIEW USING (MEMBER_ID)
-ORDER BY 3, 2
+
+# SELECT MEMBER_NAME, REVIEW_TEXT, DATE_FORMAT(REVIEW_DATE, '%Y-%m-%d') AS REVIEW_DATE
+# FROM (
+#         SELECT MEMBER_ID, MEMBER_NAME, COUNT(1) AS CNT
+#         FROM MEMBER_PROFILE
+#         JOIN REST_REVIEW USING (MEMBER_ID)
+#         GROUP BY MEMBER_ID
+#         ORDER BY CNT DESC
+#         LIMIT 1
+#     ) T
+# JOIN REST_REVIEW USING (MEMBER_ID)
+# ORDER BY 3, 2
+
+WITH ranked_members AS (
+    SELECT 
+        member_id,
+        RANK() OVER (ORDER BY COUNT(review_id) DESC) as review_rank
+    FROM rest_review
+    GROUP BY member_id
+)
+SELECT 
+    mp.member_name,
+    rr.review_text,
+    DATE_FORMAT(rr.review_date, "%Y-%m-%d") AS review_date
+FROM member_profile mp
+JOIN rest_review rr USING (member_id)
+JOIN ranked_members rm ON mp.member_id = rm.member_id
+WHERE rm.review_rank = 1
+ORDER BY rr.review_date ASC, rr.review_text ASC;
