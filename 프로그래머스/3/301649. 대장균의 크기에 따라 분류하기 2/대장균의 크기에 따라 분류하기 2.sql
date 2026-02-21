3676b061a83758a57739e6b97c44b4b9c0205ee8
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-02-21 11:02:28 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
+++ 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -1,12 +1,32 @@
--- 코드를 작성해주세요
-SELECT ID, CASE
-            WHEN RN / TOTAL <= 0.25 THEN 'CRITICAL'
-            WHEN RN / TOTAL <= 0.50 THEN 'HIGH'
-            WHEN RN / TOTAL <= 0.75 THEN 'MEDIUM'
-            ELSE 'LOW'
-            END COLONY_NAME
+SELECT ID,
+(
+    CASE
+        WHEN RK / TOTAL <= 0.25 THEN 'CRITICAL'
+        WHEN RK / TOTAL <= 0.5 THEN 'HIGH'
+        WHEN RK / TOTAL <= 0.75 THEN 'MEDIUM'
+        ELSE 'LOW'
+    END
+) AS COLONY_NAME
 FROM (
-    SELECT *, ROW_NUMBER() OVER (ORDER BY SIZE_OF_COLONY DESC) RN, COUNT(*) OVER() TOTAL
+    SELECT ID, 
+        ROW_NUMBER() OVER (ORDER BY SIZE_OF_COLONY DESC) AS RK,
+        COUNT(*) OVER () AS TOTAL # 전체 행 개수를 각 행에 붙임
     FROM ECOLI_DATA
-) t
-ORDER BY ID;
+) T
+ORDER BY 1
+
+
+# SELECT ID,
+#         CASE
+#             WHEN RN / TOTAL <= 0.25 THEN 'CRITICAL'
+#             WHEN RN / TOTAL <= 0.50 THEN 'HIGH'
+#             WHEN RN / TOTAL <= 0.75 THEN 'MEDIUM'
+#             ELSE 'LOW'
+#         END COLONY_NAME
+# FROM (
+#     SELECT ID,
+#             ROW_NUMBER() OVER (ORDER BY SIZE_OF_COLONY DESC) RN,
+#             COUNT(*) OVER() TOTAL
+#     FROM ECOLI_DATA
+# ) t
+# ORDER BY ID;
