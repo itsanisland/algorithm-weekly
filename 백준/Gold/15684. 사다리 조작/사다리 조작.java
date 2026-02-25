d4d1a89947a1bd1238d287214b64c48f25b57888
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-02-25 16:03:18 +0900

    [Gold III] Title: 사다리 조작, Time: 464 ms, Memory: 17624 KB -BaekjoonHub

--- 백준/Gold/15684. 사다리 조작/사다리 조작.java
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.java
@@ -1,64 +1,67 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Main {
 
-    public static int N, M, H, answer = 4;
-    public static boolean[][] ladder;
+    static int N, M, H;
+    static boolean[][] ladder; // [행][열]
+    static int ans = 4; // 최대 3개까지만 허용
 
     static boolean play() {
         for (int start = 1; start <= N; start++) {
-            int x = start;
-            for (int y = 1; y <= H; y++) {
-                if (ladder[y][x]) x++;
-                else if (x > 1 && ladder[y][x - 1]) x--;
+            int pos = start;
+            for (int i = 1; i <= H; i++) {
+                if (ladder[i][pos]) pos++;
+                else if (pos > 1 && ladder[i][pos - 1]) pos--;
             }
-            if (x != start) return false;
+            if (pos != start) return false;
         }
         return true;
     }
 
-    static void dfs(int cnt, int start) {
-        if (cnt > 3) return;
-        if (cnt >= answer) return;
-    
+    static void dfs(int depth, int idx) {
+        if (depth >= ans) return;
+
         if (play()) {
-            answer = cnt;
+            ans = depth;
             return;
         }
 
-        for (int i = start; i <= H; i++) {
-            for (int j = 1; j < N; j++) {
-                // 현재 위치, 왼쪽, 오른쪽에 이미 사다리가 있으면 설치 불가
-                if (ladder[i][j] || ladder[i][j - 1] || ladder[i][j + 1]) continue;
+        if (depth == 3) return;
 
-                ladder[i][j] = true;      // 사다리 심기
-                dfs(cnt + 1, i);         // 재귀 호출 (현재 행 i부터 탐색하도록 최적화)
-                ladder[i][j] = false;      // 사다리 뽑기 (복구)
-            }
+        for (int i = idx; i <= H * (N - 1); i++) {
+            int r = (i - 1) / (N - 1) + 1;
+            int c = (i - 1) % (N - 1) + 1;
+
+            if (ladder[r][c]) continue;
+            if (c > 1 && ladder[r][c - 1]) continue;
+            if (c < N - 1 && ladder[r][c + 1]) continue;
+
+            ladder[r][c] = true;
+            dfs(depth + 1, i + 1);
+            ladder[r][c] = false;
         }
     }
-    
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         H = Integer.parseInt(st.nextToken());
+
         ladder = new boolean[H + 1][N + 1];
-        
-        if (M > 0) {
-            for (int i = 0; i < M; i++) {
-                st = new StringTokenizer(br.readLine());
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
-                ladder[a][b] = true;
-            }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            ladder[a][b] = true;
         }
 
-        dfs(0, 1); // 1행부터 시작
+        dfs(0, 1);
 
-        System.out.println(answer == 4 ? -1 : answer);
+        System.out.println(ans > 3 ? -1 : ans);
     }
 }
