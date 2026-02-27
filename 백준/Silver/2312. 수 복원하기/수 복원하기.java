b4f577aeca0050974f7467cd93fcad62b9811453
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-02-27 09:48:16 +0900

    [Silver III] Title: 수 복원하기, Time: 128 ms, Memory: 16120 KB -BaekjoonHub

--- 백준/Silver/2312. 수 복원하기/수 복원하기.java
+++ 백준/Silver/2312. 수 복원하기/수 복원하기.java
@@ -4,29 +4,30 @@ import java.io.*;
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        StringBuilder sb = new StringBuilder();
+        
         int T = Integer.parseInt(br.readLine());
         
         while (T-- > 0) {
             int N = Integer.parseInt(br.readLine());
-            Map<Integer, Integer> map = new TreeMap<>();
             
             for (int i = 2; i * i <= N; i++) {
+                int cnt = 0;    
                 while (N % i == 0) {
                     N /= i;
-                    map.put(i, map.getOrDefault(i, 0) + 1);
+                    cnt++;
                 }
+                if (cnt == 0) continue;
+                sb.append(i + " " + cnt).append("\n");                
             }
 
             // 핵심: for 루프가 완전히 종료된 후, 아직 1보다 큰 N이 남아있다면 
             // 그 값은 N의 가장 큰 마지막 소인수
             if (N > 1) {
-                map.put(N, map.getOrDefault(N, 0) + 1);
-            }
-
-            for (int key : map.keySet()) {
-                System.out.println(key + " " + map.get(key));
+                sb.append(N + " " + 1).append("\n");  
             }
         }
+
+        System.out.println(sb);
     }
 }
