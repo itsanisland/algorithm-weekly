7f56ab6dc31757f504ab00206b0cde66ef921f97
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-02-28 09:56:56 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 312 ms, Memory: 18180 KB -BaekjoonHub

---
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int T = Integer.parseInt(br.readLine());
+        boolean[] eratos = new boolean[10_001];
+        eratos[0] = eratos[1] = true;
+
+        for (int i = 2; i * i <= 10_000; i++) {
+            if (eratos[i]) continue;
+            for (int j = i * i; j <= 10_000; j += i) {
+                eratos[j] = true;
+            }
+        }
+
+        while (T-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            int a = 1, b = n;
+
+            // 1 2 4 8
+            // 8 = 3 + 5
+            
+            for (int i = 2; i <= n; i++) {
+                if (eratos[i] || eratos[n - i]) continue;
+                
+                if (Math.abs(i - (n - i)) < Math.abs(a - b)) {
+                    a = i; b = n - i;
+                }
+            }
+            
+            sb.append(Math.min(a, b) + " " + Math.max(a, b)).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
