9074daf19e005b68b1b22fecb7ff42ce6e0982e2
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-03-06 09:50:39 +0900

    [Bronze I] Title: 디지털 티비, Time: 108 ms, Memory: 14100 KB -BaekjoonHub

---
+++ 백준/Bronze/2816. 디지털 티비/디지털 티비.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+    static int N, i = 0;
+    static String[] channels;
+
+    static void swap(int a, int b) {
+        String temp = channels[a];
+        channels[a] = channels[b];
+        channels[b] = temp;
+    }
+
+    static void change(String channel, int dest, BufferedWriter bw) throws IOException {
+        while (!channels[i].equals(channel)) {
+            i++;
+            bw.append("1");
+        }
+        
+        if (i >= N) {
+            while (!channels[i].equals(channel)) {
+                i--;
+                bw.append("2");
+            }
+        }
+
+        if (i < dest) {
+            while (i != dest) {
+                swap(i, i + 1);
+                i++;
+                bw.append("3");
+            }
+        } else {
+            while (i != dest) {
+                swap(i, i - 1);
+                i--;
+                bw.append("4");
+            }
+        }
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        N = Integer.parseInt(br.readLine());
+        channels = new String[N];
+        
+        for (int i = 0; i < N; i++) {
+            channels[i] = br.readLine();
+        }
+
+        change("KBS1", 0, bw);
+        change("KBS2", 1, bw);
+        
+        bw.flush();
+        bw.close();
+    }
+}
