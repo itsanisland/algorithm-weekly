23d2c8e8a3fdb591b65b939a916dc943a94dfec6
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-03-09 10:41:10 +0900

    [Silver V] Title: 임스와 함께하는 미니게임, Time: 248 ms, Memory: 27136 KB -BaekjoonHub

---
+++ 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        Map<String, Integer> map = new HashMap<>() {{
+            put("Y", 1);
+            put("F", 2);
+            put("O", 3);
+        }};
+        
+        int N = Integer.parseInt(st.nextToken());
+        String game = st.nextToken();
+        Set<String> set = new HashSet<>();
+        int ans = 0;
+        int numsOfPlayers = map.get(game);
+
+        while (N-- > 0) {
+            String name = br.readLine();
+            if (set.contains(name)) continue;
+            set.add(name);
+            ans++;
+        }
+        
+        System.out.println(ans / numsOfPlayers);
+    }
+}
