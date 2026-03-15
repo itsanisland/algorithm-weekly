1881a63ace0c8b0f464d7bc660834bc6d2be1482
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-03-15 20:41:23 +0900

    [Silver III] Title: 햄버거 분배, Time: 112 ms, Memory: 14368 KB -BaekjoonHub

--- 백준/Silver/19941. 햄버거 분배/햄버거 분배.java
+++ 백준/Silver/19941. 햄버거 분배/햄버거 분배.java
@@ -8,36 +8,27 @@ class Main {
 
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        String s = br.readLine();
-        
-        boolean[] eaten = new boolean[N];
+        char[] arr = br.readLine().toCharArray(); // String보다 char 배열이 접근이 빠름
+
         int ans = 0;
 
         for (int i = 0; i < N; i++) {
-            char ch = s.charAt(i);
-            if (ch == 'P') {
-                boolean flag = false;
-                for (int j = K; j >= 1; j--) {
-                    if (i - j >= 0 && s.charAt(i - j) == 'H' && !eaten[i - j]) {
-                        ans++;
-                        eaten[i - j] = true;
-                        flag = true;
-                        break;
-                    }
-                }
+            if (arr[i] == 'P') { // 사람을 찾으면
+                // 내 위치(i) 기준으로 -K부터 +K까지 탐색
+                // 단, 가장 왼쪽부터 먹어야 하므로 j의 시작은 i-K
+                for (int j = i - K; j <= i + K; j++) {
+                    // 범위를 벗어나는 경우 제외
+                    if (j < 0 || j >= N) continue;
 
-                if (flag) continue;
-
-                for (int j = 1; j <= K; j++) {
-                    if (i + j < N && s.charAt(i + j) == 'H' && !eaten[i + j]) {
+                    // 햄버거이고 아직 아무도 안 먹었다면
+                    if (arr[j] == 'H' && arr[j] != 'X') {
+                        arr[j] = 'X';
                         ans++;
-                        eaten[i + j] = true;
-                        break;
+                        break; // 가장 왼쪽 하나만 먹고 다음 사람으로
                     }
                 }
             }
         }
-
         System.out.println(ans);
     }
 }
