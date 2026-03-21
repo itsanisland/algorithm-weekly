7e7151e7eeb11976c41d58320db727ec716f83db
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-03-22 08:31:29 +0900

    [Silver III] Title: 가희와 키워드, Time: 2444 ms, Memory: 237376 KB -BaekjoonHub

---
+++ 백준/Silver/22233. 가희와 키워드/가희와 키워드.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        Set<String> memo = new HashSet<>();
+
+        for (int i = 0; i < N; i++) {
+            memo.add(br.readLine());
+        }
+
+        while (M-- > 0) {
+            String[] keywords = br.readLine().split(",");
+            for (String keyword : keywords) {
+                if (memo.contains(keyword)) {
+                    memo.remove((keyword));
+                }
+            }
+            System.out.println(memo.size());
+        }
+    }
+}
