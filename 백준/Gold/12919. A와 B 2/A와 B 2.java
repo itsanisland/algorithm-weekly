986693d3531bc777321bd394097e2104d0da963b
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-04-10 21:15:58 +0900

    [Gold V] Title: A와 B 2, Time: 104 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Gold/12919. A와 B 2/A와 B 2.java
+++ 백준/Gold/12919. A와 B 2/A와 B 2.java
@@ -2,39 +2,34 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-
     static int ans = 0;
-    
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         String S = br.readLine();
         String T = br.readLine();
 
-        dfs(S, T);
-        
+        dfs(T, S);
         System.out.println(ans);
     }
 
-    static void dfs(String s, String t) {
-        if (t.length() < s.length() || ans == 1) return;
-        
-        if (s.equals(t)) {
-            ans = 1;
+    static void dfs(String t, String s) {
+        // 이미 찾았거나 t가 s보다 짧아지면 종료
+        if (t.length() == s.length()) {
+            if (t.equals(s)) ans = 1;
             return;
         }
+        if (ans == 1) return;
 
-        StringBuilder sb = new StringBuilder(t);
+        // 1. 맨 뒤에 A가 있으면 A를 지워본다
         if (t.charAt(t.length() - 1) == 'A') {
-            sb.deleteCharAt(sb.length() - 1);
-            dfs(s, sb.toString());
+            dfs(t.substring(0, t.length() - 1), s);
         }
 
-        sb = new StringBuilder(t);
+        // 2. 맨 앞에 B가 있으면 B를 지우고 뒤집어본다
         if (t.charAt(0) == 'B') {
-            sb.reverse();
-            sb.deleteCharAt(sb.length() - 1);
-            dfs(s, sb.toString());
+            StringBuilder sb = new StringBuilder(t.substring(1));
+            dfs(sb.reverse().toString(), s);
         }
     }
 }
