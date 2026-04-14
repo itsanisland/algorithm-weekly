400fe93604128c6c4fd5bc04049b2c033e2891bb
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-04-14 18:57:56 +0900

    [level 2] Title: 배달, Time: 3.83 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -5,10 +5,10 @@ class Solution {
     static final int INF = Integer.MAX_VALUE;
     
     static class Road implements Comparable<Road> {
-        int node, dist;
+        int to, dist;
         
-        Road(int node, int dist) {
-            this.node = node;
+        Road(int to, int dist) {
+            this.to = to;
             this.dist = dist;
         }
         
@@ -18,26 +18,21 @@ class Solution {
     }
     
     public int solution(int N, int[][] road, int K) {        
-        int[][] graph = new int[N + 1][N + 1];
+        List<Road>[] graph = new ArrayList[N + 1];
+        for (int i = 0; i <= N; i++) graph[i] = new ArrayList<>();
         
-        for (int i = 0; i < road.length; i++) {
-            int from = road[i][0];
-            int to = road[i][1];
-            int dist = road[i][2];
-            
-            if (graph[from][to] > 0) {
-                graph[from][to] = Math.min(graph[from][to], dist);
-                graph[to][from] = Math.min(graph[to][from], dist);
-            } else {
-                graph[from][to] = dist;
-                graph[to][from] = dist;
-            }
+        for (int[] r : road) {
+            int from = r[0];
+            int to = r[1];
+            int dist = r[2];
+            graph[from].add(new Road(to, dist));
+            graph[to].add(new Road(from, dist));
         }
         
         return dijkstra(graph, N, K);
     }
     
-    int dijkstra(int[][] graph, int N, int K) {
+    int dijkstra(List<Road>[] graph, int N, int K) {
         int[] dist = new int[N + 1];
         for (int i = 0; i <= N; i++) dist[i] = INF;
         dist[1] = 0;
@@ -48,16 +43,14 @@ class Solution {
         while (!pq.isEmpty()) {
             Road cur = pq.poll();
             
-            for (int next = 1; next <= N; next++) {
-                if (graph[cur.node][next] > 0) {
-                    if (dist[cur.node] + graph[cur.node][next] < dist[next]) {
-                        dist[next] = dist[cur.node] + graph[cur.node][next];
-                        pq.offer(new Road(next, dist[next]));
-                    }
+            for (Road next : graph[cur.to]) {
+                if (dist[cur.to] + next.dist < dist[next.to]) {
+                    dist[next.to] = dist[cur.to] + next.dist;
+                    pq.offer(new Road(next.to, dist[next.to]));
                 }
             }
         }
-
+        
         int cnt = 0;
         for (int i = 1; i <= N; i++) {
             if (dist[i] <= K) cnt++;
