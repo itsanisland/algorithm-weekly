aafe21cfe64c0332420b15f37395981c02a309c2
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-04-21 16:16:50 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 92.11 ms, Memory: 96.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> pMap = new HashMap<>();
+        
+        for (String name : participant) {
+            pMap.put(name, pMap.getOrDefault(name, 0) + 1);
+        }
+        
+        for (String name : completion) {
+            pMap.put(name, pMap.getOrDefault(name, 0) - 1);
+            
+            if (pMap.get(name) == 0) {
+                pMap.remove(name);
+            }
+        }
+        
+        String answer = "";
+        
+        for (String key : pMap.keySet()) {
+            answer = key;
+        }
+        
+        return answer;
+    }
+}
