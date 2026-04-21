3fb869e6c12980583f6b8fd92795b70593bddcd6
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-04-21 16:22:50 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 80.45 ms, Memory: 97.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,15 +2,19 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
+        // {선수이름:참여횟수}
         Map<String, Integer> pMap = new HashMap<>();
         
+        // 특정 선수의 참여 횟수 세기(동명이인 고려)
         for (String name : participant) {
             pMap.put(name, pMap.getOrDefault(name, 0) + 1);
         }
         
+        // 실제 완주한 선수의 횟수 제외
         for (String name : completion) {
             pMap.put(name, pMap.get(name) - 1);
             
+            // 완주 완료한 선수는 Map에서 제거
             if (pMap.get(name) == 0) {
                 pMap.remove(name);
             }
@@ -18,6 +22,7 @@ class Solution {
         
         String answer = "";
         
+        // 완주하지 않은 선수 1명만 Map에 남아있음
         for (String key : pMap.keySet()) {
             answer = key;
         }
