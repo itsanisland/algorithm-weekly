a66f6d96eb1c91ccf631b24be1d75a3368792532
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-04-22 13:13:41 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 31.37 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 같은 숫자는 싫어 - 12906 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906) 
 
 ### 성능 요약
 
-메모리: 117 MB, 시간: 45.35 ms
+메모리: 117 MB, 시간: 31.37 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 21일 15:36:19
+2026년 04월 22일 13:13:37
 
 ### 문제 설명
 
--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,19 +2,23 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int[] arr) {
-        // 슬라이딩 윈도우
-        
+        // 투포인터
         List<Integer> list = new ArrayList<>();
-        int left = 0, right = 0;
+        int left = 0;
         int n = arr.length;
         
-        while (left < n && right < n) {
-            if (arr[left] == arr[right]) {
-                list.add(arr[left]);
-                while (right < n && arr[left] == arr[right]) right++;
-            } else {
-                left++;
+        while (left < n) {
+            // 1. 현재 숫자를 리스트에 추가 (새로운 그룹의 시작)
+            list.add(arr[left]);
+
+            int right = left + 1;
+            // 2. 현재 숫자(left)와 같은 숫자가 아닐 때까지 right를 이동
+            while (right < n && arr[left] == arr[right]) {
+                right++;
             }
+
+            // 3. left를 다음 새로운 숫자가 시작되는 위치(right)로 점프!
+            left = right;
         }
         
         int[] answer = new int[list.size()];
