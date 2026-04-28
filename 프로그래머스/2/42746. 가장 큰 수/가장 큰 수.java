eb25779654ea14ccb2d19db1bb0c20538a485043
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-04-28 17:24:01 +0900

    [level 2] Title: 가장 큰 수, Time: 262.58 ms, Memory: 141 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,22 +2,27 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        String answer = "";
+        StringBuilder answer = new StringBuilder();
         int n = numbers.length;
         String[] strArr = new String[n];
         
+        // 숫자 배열 -> 문자열 배열
         for (int i = 0 ; i < n; i++) {
             strArr[i] = "" + numbers[i];
         }
         
+        // 문자열을 비교하여 정렬
         Arrays.sort(strArr, (a, b) -> {
             return (b + a).compareTo(a + b);
         });
         
+        // 모든 숫자가 0인 경우 처리
+        if (strArr[0].equals("0")) return "0";
+        
         for (String str : strArr) {
-            answer += str;
+            answer.append(str);
         }
         
-        return answer.charAt(0) == '0' ? "0" : answer;
+        return answer.toString();
     }
 }
