2318796e5fec2ba2c8aa49f53d2af40cfa346d78
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-05-05 21:49:29 +0900

    [level 1] Title: 신규 아이디 추천, Time: 14.25 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,56 +1,29 @@
 class Solution {
     public String solution(String new_id) {
-        new_id = new_id.toLowerCase();
+        String str = new_id.toLowerCase();
         
-        StringBuilder sb = new StringBuilder();
+        str = str.replaceAll("[^a-z0-9-_.]", "");
+
+        str = str.replaceAll("[.]{2,}", ".");
         
-        for (int i = 0; i < new_id.length(); i++) {
-            char ch = new_id.charAt(i);
-            if ((ch - 'a' >= 0 && ch - 'z' <= 0) ||
-                (ch - '0' >= 0 && ch - '9' <= 0) ||
-                ch == '-' ||
-                ch == '_' ||
-                ch == '.') {
-                sb.append(ch);
-            }
+        str = str.replaceAll("^[.]|[.]$", "");
+        
+        if (str.isEmpty()) {
+            str = "a";
         }
 
-        for (int i = 1; i < sb.length(); i++) {
-            if (sb.charAt(i) == '.' && sb.charAt(i - 1) == sb.charAt(i)) {
-                sb.deleteCharAt(i);
-                i--;
+        if (str.length() >= 16) {
+            str = str.substring(0, 15);
+            str = str.replaceAll("^[.]|[.]$", "");
+        }
+        
+        if (str.length() <= 2) {
+            char last = str.charAt(str. length() - 1);
+            while (str.length() < 3) {
+                str += last;
             }
         }
         
-        // 문자열이 비어있지 않은지 먼저 확인
-        if (sb.length() > 0 && sb.charAt(0) == '.') {
-            sb.deleteCharAt(0);
-        }
-
-        // 앞의 작업으로 인해 문자열이 비게 될 수 있으므로 다시 한번 길이 확인
-        if (sb.length() > 0 && sb.charAt(sb.length() - 1) == '.') {
-            sb.deleteCharAt(sb.length() - 1);
-        }
-        
-        if (sb.length() == 0) {
-            sb.append("a");
-        }
-        
-        if (sb.length() >= 16) {
-            sb.delete(15, sb.length());
-            
-            if (sb.length() > 0 && sb.charAt(sb.length() - 1) == '.') {
-                sb.deleteCharAt(sb.length() - 1);
-            }
-        }
-        
-        if (sb.length() <= 2) {
-            char last = sb.charAt(sb.length() - 1);
-            while (sb.length() < 3) {
-                sb.append(last);
-            }
-        }
-        
-        return sb.toString();
+        return str;
     }
 }
