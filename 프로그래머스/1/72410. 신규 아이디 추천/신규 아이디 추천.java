946a761d683aa6faec13904f81cdd94d4e5c3a03
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-05-05 21:54:12 +0900

    [level 1] Title: 신규 아이디 추천, Time: 17.34 ms, Memory: 88.7 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -3,10 +3,15 @@ class Solution {
         String str = new_id.toLowerCase();
         
         str = str.replaceAll("[^a-z0-9-_.]", "");
+        // [^]: 대괄호 안에 있는 문자들을 제외한 나머지 전부
 
         str = str.replaceAll("[.]{2,}", ".");
+        // {2,}: 2번 이상 반복되는 경우
         
         str = str.replaceAll("^[.]|[.]$", "");
+        // ^[.]: 문자열의 시작 (.만 쓰면 모든 문자 의미)
+        // |: OR 연산자 -> 둘 중 하나라도 만족하면 해당 부분 선택
+        // [.]$: 문자열의 끝
         
         if (str.isEmpty()) {
             str = "a";
@@ -14,7 +19,7 @@ class Solution {
 
         if (str.length() >= 16) {
             str = str.substring(0, 15);
-            str = str.replaceAll("^[.]|[.]$", "");
+            str = str.replaceAll("[.]$", "");
         }
         
         if (str.length() <= 2) {
