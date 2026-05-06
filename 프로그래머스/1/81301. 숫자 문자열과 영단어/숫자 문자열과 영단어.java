1621e8bbcdaab25bc648c3082ac2f9548f13c1fa
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-05-06 16:18:35 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 8.73 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -3,33 +3,12 @@ import java.util.*;
 class Solution {
     public int solution(String s) {
         String answer = "";
-        List<String> nums = new ArrayList();
-        nums.add("zero");
-        nums.add("one");
-        nums.add("two");
-        nums.add("three");
-        nums.add("four");
-        nums.add("five");
-        nums.add("six");
-        nums.add("seven");
-        nums.add("eight");
-        nums.add("nine");
+        String[] nums = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
         
-        String word = "";
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (ch - '0' >= 0 && ch - '9' <= 0) {
-                answer += ch;
-                continue;
-            }
-            
-            word += ch;
-            if (nums.contains(word)) {
-                answer += nums.indexOf(word);
-                word = "";
-            }
+        for (int i = 0; i < nums.length; i++) {
+            s = s.replaceAll(nums[i], "" + i);
         }
         
-        return Integer.parseInt(answer);
+        return Integer.parseInt(s);
     }
 }
