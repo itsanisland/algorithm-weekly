635541c24067da9a75f98010b8dfc97c05921caa
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-07-01 17:17:45 +0900

    [level 1] Title: 하샤드 수, Time: 0.20 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean solution(int x) {
+        int sum = 0;
+        int digit = x;
+        while (digit > 0) {
+            sum += digit % 10;
+            digit /= 10;
+        }
+        System.out.println(sum);
+        return x % sum == 0;
+    }
+}
