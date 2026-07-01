55847c8d6ac49472e7c59983b765d358e21179e7
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-07-01 17:07:41 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.05 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public long solution(long n) {
+        long answer = -1;
+        
+        double sqrt = Math.sqrt(n);
+        
+        if (sqrt == (int) sqrt) {
+            answer = (long) Math.pow(sqrt + 1, 2);
+        }
+        
+        return answer;
+    }
+}
