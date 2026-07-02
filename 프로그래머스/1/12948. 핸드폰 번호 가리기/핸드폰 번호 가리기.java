a2b5cfa615e2195baf4a0068cab21cac4465e71a
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-07-02 18:11:57 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.04 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String phone_number) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < phone_number.length(); i++) {
+            sb.append((i < phone_number.length() - 4) ? "*" : phone_number.charAt(i));
+        }
+        return sb.toString();
+    }
+}
