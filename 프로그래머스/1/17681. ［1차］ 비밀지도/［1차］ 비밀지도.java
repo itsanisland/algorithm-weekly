96ce5696e52586a274b6c14582344f64da55aeab
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-07-08 14:52:08 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.35 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,28 +1,18 @@
 class Solution {
     
-    private String convertToBinary(int decimal, int n) {
-        StringBuilder sb = new StringBuilder();
-        
-        while (decimal > 0) {
-            sb.append(decimal % 2);
-            decimal /= 2;
-        }
-        
-        int length = n - sb.length();
-        
-        for (int i = 0; i < length; i++) {
-            sb.append("0");
-        }
-        
-        return sb.reverse().toString();
-    }
-    
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
         
         for (int i = 0; i < n; i++) {
-            String binary = convertToBinary(arr1[i] | arr2[i], n);
-            answer[i] = binary.replaceAll("0", " ").replaceAll("1", "#");
+            int num = arr1[i] | arr2[i];
+            StringBuilder sb = new StringBuilder();
+            
+            for (int j = 0; j < n; j++) {
+                sb.append(num % 2 == 0 ? " " : "#");
+                num /= 2;
+            }
+            
+            answer[i] = sb.reverse().toString().replaceAll("0", " ").replaceAll("1", "#");
         }
 
         return answer;
