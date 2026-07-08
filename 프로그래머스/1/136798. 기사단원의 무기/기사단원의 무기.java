0fb3d53fa842aa131feab889fbd2f81618301399
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-07-08 15:07:07 +0900

    [level 1] Title: 기사단원의 무기, Time: 7846.60 ms, Memory: 80.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,23 @@
+class Solution {
+    
+    private int getDivisorCount(int num) {
+        int cnt = 0;
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                cnt += 1;
+            }
+        }
+        return cnt;
+    }
+    
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+        
+        for (int i = 1; i <= number; i++) {
+            int divisorCount = getDivisorCount(i);
+            answer += divisorCount > limit ? power : divisorCount;
+        }
+        
+        return answer;
+    }
+}
