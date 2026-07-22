46d039aa6fd02bc5f3b250baa1898336483873d7
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-07-22 15:33:33 +0900

    [level 1] Title: 노란불 신호등, Time: 21.19 ms, Memory: 90.6 MB -BaekjoonHub

--- 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.java
+++ 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.java
@@ -1,7 +1,23 @@
 class Solution {
+    
+    private int lcm(int a, int b) {
+        return b > 0 ? lcm(b, a % b) : a;
+    }
+    
+    private int gcd(int a, int b) {
+        return a * b / lcm(a, b);
+    }
+    
     public int solution(int[][] signals) {
         int n = signals.length;
-        int m = 3_200_001;
+        int m = signals[0][0] + signals[0][1] + signals[0][2];
+        
+        for (int i = 1; i < n; i++) {
+            m = gcd(m, signals[i][0] + signals[i][1] + signals[i][2]);
+        }
+        
+        m++;
+        
         boolean[][] isYellow = new boolean[n][m];
         
         for (int i = 0; i < n; i++) {
