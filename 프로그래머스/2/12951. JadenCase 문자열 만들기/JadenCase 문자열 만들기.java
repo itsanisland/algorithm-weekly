420028a41cb0f4c902d3d0f64e8c261c6dd4baf9
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-07-24 21:27:04 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.39 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,30 +1,13 @@
 class Solution {
     public String solution(String s) {
         StringBuilder answer = new StringBuilder();
-        StringBuilder temp = new StringBuilder();
+        boolean ck = true;
         
-        for (char c : s.toCharArray()) {
-            if (c == ' ') {
-                if (!temp.isEmpty()) {
-                    String str = temp.toString().toLowerCase();
-                    char first = Character.toUpperCase(str.charAt(0));
-                    answer.append(first + str.substring(1, str.length()));
-                }
+        for (String str : s.split("")) {
+            answer.append(ck ? str.toUpperCase() : str.toLowerCase());
+            ck = str.equals(" ");
+        }
 
-                temp = new StringBuilder();
-                answer.append(' ');
-                continue;
-            }
-            
-            temp.append(c);
-        }
-        
-        if (!temp.isEmpty()) {
-            String str = temp.toString().toLowerCase();
-            char first = Character.toUpperCase(str.charAt(0));
-            answer.append(first + str.substring(1, str.length()));
-        }
-        
         return answer.toString();
     }
 }
